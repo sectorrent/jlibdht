2ba77cf2f792cc80deecb8eaacd5e9e5f6d328f9
DrBrad <brad@bradeagle.com>
2025-01-09 18:05:53 -0700

    Revert

--- src/org/sectorrent/jlibdht/refresh/RefreshHandler.java
+++ src/org/sectorrent/jlibdht/refresh/RefreshHandler.java
@@ -28,10 +28,6 @@ public class RefreshHandler {
     }
 
     public void start(){
-        start(0);
-    }
-
-    public void start(long delay){
         if(isRunning()){
             throw new IllegalArgumentException("Refresh has already started.");
         }
@@ -49,7 +45,7 @@ public class RefreshHandler {
                 }
             };
 
-            refreshTimer.schedule(refreshTimerTask, delay, refreshTime); //MAKE DELAY LONG, HOWEVER PERIOD AROUND 1 HOUR
+            refreshTimer.schedule(refreshTimerTask, refreshTime, refreshTime); //MAKE DELAY LONG, HOWEVER PERIOD AROUND 1 HOUR
         }
     }
 
